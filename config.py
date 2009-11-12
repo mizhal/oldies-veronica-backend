053738502772d87c821830584f967350aa4ce2c0
mizhal <mizhal@localhost.localdomain>
2009-11-12 03:07:08 +0100

    restauracion

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+#############3
+## Configuraciones
+
 db_string = "host=127.0.0.1 user=vigilante password=Casablancas dbname=veronica"
 
 credentials = ("127.0.0.1","veronica","vigilante","Casablancas")
@@ -5,3 +8,4 @@ credentials = ("127.0.0.1","veronica","vigilante","Casablancas")
 xapian_news_base = "/var/mik6/Base/indexes/news"
 
 logfile = "/tmp/veronica-error.log"
+
