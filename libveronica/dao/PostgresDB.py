e58bdd8dff1430181a1093ccf46b810f64986f62
mizhal <mizhal@GWION>
2009-12-10 12:39:23 +0100

    Añadido acceso privilegiado a la base de datos y funcionalidad para leer archivos .ini ademas de un archivo de configuracion para la base de datos

--- libveronica/dao/PostgresDB.py
+++ libveronica/dao/PostgresDB.py
@@ -1,19 +1,35 @@
-from psycopg2 import connect
+from os.path import join, split
 from time import strftime
+from datetime import datetime
+from exceptions import Exception
 
-from ..config import credentials
+from md5 import md5
 
-class PostgresDB:
+from psycopg2 import connect
+
+from ..utils.readini import IniFile
+
+local_dir, filename = split(__file__)[0]
+
+config = IniFile(join(local_dir, "dbconfig.ini"))
+
+SESSION_TIMEOUT = int(config.get("session.timeout"))
+
+class PostgresDBReader:
 	singleton = None
 	
 	@staticmethod
 	def getInstance():
-		if PostgresDB.singleton is None:
-			PostgresDB.singleton = PostgresDB()
-		return PostgresDB.singleton
+		if PostgresDBReader.singleton is None:
+			PostgresDBReader.singleton = PostgresDBReader()
+		return PostgresDBReader.singleton
 	
 	def __init__(self):
-		self.con = connect("host=%s dbname=%s user=%s password=%s"%credentials)
+		user = config.get("reader.user")
+		host = config.get("reader.host")
+		password = config.get("reader.password")
+		dbname = config.get("reader.dbname")
+		self.con = connect("host=%s dbname=%s user=%s password=%s"%(host, dbname, user, password))
 		
 	def connect(self, host, dbname, user, passwd):
 		pass
@@ -32,3 +48,57 @@ class PostgresDB:
 		
 	def __del__(self):
 		self.con.close()
+		
+	SESSION_TOKENS = {}
+		
+	def _genSessionToken(self):
+		letters = []
+		for i in range(128):
+			letters.append(random.randint(ord("A"), ord("Z")))
+		return "".join(letters)
+		
+	def openSession(self, user, password):
+		cur = self.cursor()
+		cur.execute("select * from users where user=? and password=?", user, password)
+		if len(cur.fetchall()) > 0:
+			token = self._genSessionToken()
+			while PostgresDBReader.SESSION_TOKENS.has_key(token):
+				token = self._genSessionToken()
+			
+			PostgresDBReader.SESSION_TOKENS[token] = (user, datetime.now())
+				
+			return token
+		else
+			return False
+			
+	def validateUser(self, user, session_token):
+		q = PostgresDBReader.SESSION_TOKENS.get(session_token, None)
+		if q:
+			if q[0] == user and (datetime.now() - q[2]).seconds < SESSION_TIMEOUT:
+				PostgresDBReader.SESSION_TOKENS[token] = (user, datetime.now())
+				return True
+			else:
+				return False
+		
+			
+class NotEnoughPrivileges(Exception):
+	pass
+
+class PostgresDBPrivileged(PostgresDBReader):
+	singleton = None
+	
+	@staticmethod
+	def getInstance(user, session_token):
+		if PostgresDBPrivileged.singleton is None:
+			PostgresDBPrivileged.singleton = PostgresDBPrivileged()
+		if self.validateUser(user, session_token):
+			return PostgresDBPrivileged.singleton
+		else:
+			raise NotEnoughPrivileges()
+	
+	def __init__(self):
+		user = config.get("privileged.user")
+		host = config.get("privileged.host")
+		password = config.get("privileged.password")
+		dbname = config.get("privileged.dbname")
+		self.con = connect("host=%s dbname=%s user=%s password=%s"%(host, dbname, user, password))
