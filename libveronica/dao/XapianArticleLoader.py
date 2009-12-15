0d94295a268165b3d15ab3b8012872518025e8b7
mizhal <mizhal@Alvi>
2009-12-15 17:47:14 +0100

    ++

--- libveronica/dao/XapianArticleLoader.py
+++ libveronica/dao/XapianArticleLoader.py
@@ -214,8 +214,6 @@ class XapianArticleLoader:
         
         enquire = xapian.Enquire(self.read_db)
         
-        for term in termbag:
-            query = xapian.Query(xapian.OP_OR)
         query = " OR ".join(termbag)
         query = query = self.parser.parse_query(query.encode("utf8"),
                                                  DEFAULT_SEARCH_FLAGS)
