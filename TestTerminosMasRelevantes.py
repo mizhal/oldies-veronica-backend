59b6d7f1d8eb5aea1285523d0cdca1ca0ad17c9a
mizhal <mizhal@Alvi>
2009-12-15 16:35:10 +0100

    ++

--- TestTerminosMasRelevantes.py
+++ TestTerminosMasRelevantes.py
@@ -1,4 +1,5 @@
 ''' prueba de la funcionalidad de terminos mas relevantes '''
+from sys import stdin
 
 from libveronica.dao.XapianArticleLoader import XapianArticleLoader
 from libveronica.dao.PostgreSQLArticleLoader import PostgreSQLArticleLoader
@@ -10,14 +11,14 @@ artloader = PostgreSQLArticleLoader()
 while 1:
     print "prueba un articulo"
     id = input()
-    art = artloader.loadAllArticles(400,1)[0]
+    art = artloader.loadAllArticles(id,1)[0]
     
     print "Contenido del articulo"
     print "-----------------------------"
     print art.content
     
     print "es este archivo correcto?"
-    res = input()
+    res = stdin.readline()
     if res == 'S' or res == 's':
         break
 
